9fafb55d4d4d61bcb6f87e83510afe97fac4eb34
Arijit <arijit1.manna@adamastech.in>
2023-03-15 12:29:43 +0530

    project added

---
+++ ECommPortalDataAccess/ECommPortalDBAccess.cs
@@ -0,0 +1,64 @@
+﻿using Entities;
+using Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Numerics;
+using Utilities;
+
+namespace ECommPortalDataAccess
+{
+    public class ECommPortalDBAccess : IEcommPortalDBAccess
+    {
+        private string _conn;
+
+        public ECommPortalDBAccess(IConfiguration configuration)
+        {
+            _conn = configuration.GetConnectionString("EcommPortalConnectionString");
+        }
+        public Plan GetPlanDetails()
+        {
+            DataHelper dh = new DataHelper(_conn);
+            DataSet ds = new DataSet();
+
+            Plan plan = new Plan();
+
+            int j = 0;
+            try
+            {
+                SqlParameter[] sqlparams = new SqlParameter[1];
+                sqlparams[0] = new SqlParameter("@PlanID", 1);
+
+                ds = dh.ExecuteDataSet("ECOMMERCE.uspGetPlanDetailsForAdmin", CommandType.StoredProcedure, sqlparams);
+
+                if (ds != null && ds.Tables.Count > 0)
+                {
+                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+
+                        plan.PlanID = Convert.ToInt32(ds.Tables[0].Rows[i]["PlanID"]);
+                        plan.PlanName = Convert.ToString(ds.Tables[0].Rows[i]["PlanName"]);
+
+                    }
+                }
+            }
+            finally
+            {
+                if (dh != null)
+                {
+                    if (dh.DataConn != null)
+                    {
+                        dh.DataConn.Close();
+                    }
+                }
+            }
+
+            return plan;
+
+        }
+
+    }
+}
---
+++ Entities/Plan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Plan
+    {
+        public int PlanID { get; set; }
+        public string PlanCode { get; set; }
+        public string PlanName { get; set; }
+        public string PlanDesc { get; set; }
+        public string BrandIDList { get; set; }
+        //public int BrandID { get; set; }
+        public List<Brand> BrandDetails { get; set; } = new();
+        public bool IsPublished { get; set; }
+        public int StatusID { get; set; }
+        public string PlanImage { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public List<PlanConfig> ConfigList { get; set; } = new List<PlanConfig>();
+        public List<int> CategoryIDList { get; set; } = new();
+
+        public int PlanLanguageID { get; set; } //susmita
+        public string PlanLanguageName { get; set; } //susmita
+    }
+}
